d332bf9bb4fd6b88fff875d6c2ff94a8831f90c4
laravell2004 <agungnugroho16@sma.belajar.id>
2023-11-03 23:20:39 +0700

    initial

--- src/pages/Background/index.js
+++ src/pages/Background/index.js
@@ -1,2 +1,11 @@
 console.log('This is the background page.');
 console.log('Put the background scripts here.');
+
+console.log('hallo');
+let linkBlock = [];
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.type == 'product') {
+    linkBlock.push(request.data);
+  }
+  console.log(linkBlock);
+});
--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,35 @@
-import { printLine } from './modules/print';
+function addBlock() {
+  const container = document.querySelector('.css-rjanld');
+  if (container) {
+    const productContainer = container.querySelector(
+      "[data-testid='divSRPContentProducts']"
+    );
+    if (productContainer) {
+      Array.from(productContainer.children).map((item, index) => {
+        console.log(item);
+        Array.from(item.children).map((product, key) => {
+          if (product.classList.contains('css-llwpbs')) {
+            const cardProduct = product.querySelector(
+              '.css-54k5sq .css-uwyh54 .css-qa82pd .css-1c4umxf .css-1izdl9e'
+            );
+            const button = document.createElement('button');
+            button.innerText = 'Block';
+            cardProduct.append(button);
+            button.onclick = () => {
+              cardProduct.style.display = 'none';
+              chrome.runtime.sendMessage({
+                type: 'product',
+                data: {
+                  item: product.querySelector('.css-1f2quy8 a').href,
+                  index: key,
+                },
+              });
+            };
+          }
+        });
+      });
+    }
+  }
+}
 
-console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
+document.onreadystatechange(addBlock());
